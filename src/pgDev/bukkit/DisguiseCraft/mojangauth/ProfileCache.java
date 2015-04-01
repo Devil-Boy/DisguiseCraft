24559ce316bf59254a7ce6ccd630f504d4a2fde4
Devil Boy <devilboy@pinoygamers.info>
2015-03-31 23:32:31 -0700

    Removed (hopefully) unnecessary uses of reflection in getting profiles

--- src/pgDev/bukkit/DisguiseCraft/mojangauth/ProfileCache.java
+++ src/pgDev/bukkit/DisguiseCraft/mojangauth/ProfileCache.java
@@ -1,19 +1,15 @@
 package pgDev.bukkit.DisguiseCraft.mojangauth;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 
-import net.minecraft.server.v1_8_R2.EntityHuman;
 
 import com.mojang.authlib.GameProfile;
 
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.craftbukkit.v1_8_R2.CraftOfflinePlayer;
 import org.bukkit.craftbukkit.v1_8_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
@@ -24,43 +20,16 @@ public class ProfileCache {
 	// The database for storing profiles
 	Map<String, GameProfile> cache;
 	
-	// This is the GameProfile field in the player entity
-	Field playerProfileField;
-	Field offlinePlayerProfileField;
-	
 	public ProfileCache() throws Exception {
 		// Instantiate cache
 		cache = new ConcurrentHashMap<String, GameProfile>();
-		
-		// Search for GameProfile field by type
-		for (Field field : EntityHuman.class.getDeclaredFields()) {
-			if (field.getType() == GameProfile.class && !Modifier.isStatic(field.getModifiers())) {
-				playerProfileField = field;
-			}
-		}
-		for (Field field : CraftOfflinePlayer.class.getDeclaredFields()) {
-			if (field.getType() == GameProfile.class && !Modifier.isStatic(field.getModifiers())) {
-				offlinePlayerProfileField = field;
-			}
-		}
-		
-		// Fall-back on hardcoded GameProfile field name
-		if (playerProfileField == null) {
-			playerProfileField = EntityHuman.class.getDeclaredField("i");
-		}
-		if (offlinePlayerProfileField == null) {
-			offlinePlayerProfileField = CraftOfflinePlayer.class.getDeclaredField("profile");
-		}
-		
-		// Set field accessible
-		playerProfileField.setAccessible(true);
 	}
 	
 	public GameProfile cache(String playerName) {
 		// Check if online
 		Player onlinePlayer = Bukkit.getPlayer(playerName);
 		if (onlinePlayer != null) {
-			GameProfile profile = extractProfile(onlinePlayer);
+			GameProfile profile = ((CraftPlayer) onlinePlayer).getProfile();
 			cache.put(playerName, profile);
 			return profile;
 		}
@@ -74,7 +43,7 @@ public class ProfileCache {
 			UUID uid = uResponse.get(playerName);
 			if (uid != null) {
 				// Search for GameProfile using UUID
-				return extractProfile(Bukkit.getOfflinePlayer(uid));
+				return ((CraftOfflinePlayer) Bukkit.getOfflinePlayer(uid)).getProfile();
 				
 				/*
 				ProfileFetcher pFetcher = new ProfileFetcher(Arrays.asList(uid));
@@ -118,23 +87,5 @@ public class ProfileCache {
 			uid = UUID.randomUUID();
 		} while (cache.keySet().contains(uid));
 		return uid;
-	}
-	
-	public GameProfile extractProfile(Player player) {
-		try {
-			return (GameProfile) playerProfileField.get(((CraftPlayer) player).getHandle());
-		} catch (Exception e) {
-			DisguiseCraft.logger.log(Level.WARNING, "Could not properly extract GameProfile for player: " + player.getName());
-			return new GameProfile(player.getUniqueId(), player.getName());
-		}
-	}
-	
-	public GameProfile extractProfile(OfflinePlayer player) {
-		try {
-			return (GameProfile) offlinePlayerProfileField.get((CraftOfflinePlayer) player);
-		} catch (Exception e) {
-			DisguiseCraft.logger.log(Level.WARNING, "Could not properly extract GameProfile for offline player: " + player.getName());
-			return new GameProfile(player.getUniqueId(), player.getName());
-		}
 	}
 }
