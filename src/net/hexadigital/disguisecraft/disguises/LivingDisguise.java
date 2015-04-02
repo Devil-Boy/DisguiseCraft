7701c2ed83c0fe82a7d24322852f8ff5e27ff66a
Devil Boy <devilboy@pinoygamers.info>
2015-04-02 13:49:20 -0700

    Using Strings for item types in new system

--- src/net/hexadigital/disguisecraft/disguises/LivingDisguise.java
+++ src/net/hexadigital/disguisecraft/disguises/LivingDisguise.java
@@ -15,129 +15,63 @@ public interface LivingDisguise extends EntityDisguise {
 
 	// TODO: Add methods for potion effects
 	
-	// TODO: All methods derived from EntityEquipment had ItemStack replaced with int
-	
-	/**
-	 * Clears the disguise of all armor and held items
-	 */
-	void clear();
-	
-	/**
-	 * Gets a copy of all worn armor
-	 */
-	int[] getArmorContents();
-	
-	/**
-	 * Gets a copy of the boots currently being worn by teh entity
-	 */
-	int getBoots();
-	
-	/**
-	 * Gets the chance of the boots being dropped upon this creatures death
-	 */
-	float getBootsDropChance();
-	
-	/**
-	 * Gets a copy of the chestplate currently being worn by the entity
-	 */
-	int getChestPlate();
-	
-	/**
-	 * Gets the chance of the chest plate being dropped upon this creatures death
-	 */
-	float getChestPlateDropChance();
-	
 	/**
 	 * Gets a copy of the helmet currently being worn by the entity
+	 * @return The name of the item being used as a helmet
 	 */
-	int getHelmet();
-	
-	/**
-	 * Gets the chance of the helmet being dropped upon this creatures death
-	 */
-	float getHelmetDropChance();
-	
-	/**
-	 * Get the entity that this inventory belongs to
-	 * 
-	 * in the java docs, this is Entity, not EntityDisguise.  And its found in EntityEquipment
-	 * we decided to put the EE methods into our LivingDisguise interface
-	 */
-	EntityDisguise getHolder();
-	
-	/**
-	 * Gets a copy of the item the entity is currently holding
-	 */
-	int getItemInHand();
-	
-	/**
-	 * Gets the chance of the currently held item being dropped upon this creatures death
-	 */
-	float getItemInHandDropChance();
-	
-	/**
-	 *Gets a copy of the leggings currently being worn by the entity
-	 */
-	int getLeggings();
-	
-	/**
-	 * Gets the chance of the currently worn leggings being dropped upon this creatures death
-	 */
-	float getLeggingsDropChance();
-	
-	/**
-	 * Sets the entities armor to the provided array of ints
-	 */
-	void setArmorContents(int[] items);
-	
-	/**
-	 * Sets the boots worn by the entity
-	 */
-	void setBoots(int boots);
-	
-	/**
-	 * Sets the chance of the boots being dropped upon this creatures death
-	 */
-	void setBootsDropChance(float chance);
-	
-	/**
-	 * Sets the chest plate worn by the entity
-	 */
-	void setChestPlate(int chestplate);
-	
-	/**
-	 * Sets the chance of the chest plate being dropped upon this creatures death
-	 */
-	void setChestPlateDropChance(float chance);
+	String getHelmet();
 	
 	/**
 	 * Sets the helmet worn by the entity
+	 * @param The name of the item for the disguise to use as a helmet
 	 */
-	void setHelmet(int helmet);
+	void setHelmet(String itemName);
 	
 	/**
-	 * Sets the chance of the helmet being dropped upon this creatures death
+	 * Gets a copy of the chestplate currently being worn by the entity
+	 * @return The name of the item being used as a chest plate
 	 */
-	void setHelmetDropChance(float chance);
+	String getChestPlate();
 	
 	/**
-	 * Sets the item the entity is holding
+	 * Sets the chest plate worn by the entity
+	 * @param The name of the item for the disguise to use as a chestplate
 	 */
-	void setItemInHand(int stack);
+	void setChestPlate(String itemName);
 	
 	/**
-	 * Sets the chance of the item this creature is currently holding being dropped upon this creatures death
+	 * Gets a copy of the leggings currently being worn by the entity
+	 * @return The name of the item being used as leggings
 	 */
-	void setItemInHandDropChance(float chance);
+	String getLeggings();
 	
 	/**
 	 * Sets the leggings worn by the entity
+	 * @param The name of the item for the disguise to use as leggings
 	 */
-	void setLeggings(int leggings);
+	void setLeggings(String itemName);
 	
 	/**
-	 * Sets the chance of the leggings being dropped upon this creatures death
+	 * Gets a copy of the boots currently being worn by the entity
+	 * @return The name of the item being used as boots
 	 */
-	void setLeggingsDropChance(float chance);
+	String getBoots();
 	
+	/**
+	 * Sets the boots worn by the entity
+	 * @param The name of the item for the disguise to use as boots
+	 */
+	void setBoots(String itemName);
+	
+	/**
+	 * Gets a copy of the item the entity is currently holding
+	 * @return The name of the item being held
+	 */
+	String getItemInHand();
+	
+	/**
+	 * Sets the item the entity is holding
+	 * @param The name of the item for the disguise to hold in hand
+	 */
+	void setItemInHand(String itemName);
 }
