82cd89428040f2239256d47bdf0091aa12503860
Kervin Sam <kwsam@ucsd.edu>
2015-04-02 17:54:16 -0700

    Added location functions to EntityDisguise Created method for moving entity disguises

--- src/net/hexadigital/disguisecraft/disguises/EntityDisguise.java
+++ src/net/hexadigital/disguisecraft/disguises/EntityDisguise.java
@@ -16,6 +16,36 @@ public interface EntityDisguise extends Disguise {
 	int getEntityId();
 	
 	/**
+	 * Gets the X position of this entity disguise
+	 * @return A double representing this disguise's block distance from the map origin in the X axis
+	 */
+	double getX();
+	
+	/**
+	 * Gets the Y position of this entity disguise
+	 * @returnA double representing this disguise's block distance from the map origin in the vertical axis
+	 */
+	double getY();
+	
+	/**
+	 * Gets the Z position of this entity disguise
+	 * @return A double representing this disguise's block distance from the map origin in the Z axis
+	 */
+	double getZ();
+	
+	/**
+	 * Gets the yaw rotation of this entity disguise
+	 * @return A float representing the degree of rotation for this entity disguise going clockwise
+	 */
+	float getYaw();
+	
+	/**
+	 * Gets the pitch angle of this entity disguise
+	 * @return A float representing the vertical angle (in degrees) of this entity disguise with positive being upward
+	 */
+	float getPitch();
+	
+	/**
 	 * Gets the entity ID of the entity or disguise riding this disguise
 	 * @return A unique ID
 	 */
